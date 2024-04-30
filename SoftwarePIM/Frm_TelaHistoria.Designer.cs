73529eb253899e27dc52b8bc4f747ef7d13e1882
Matheus Costa <contato.matheusccp5@gmail.com>
2024-04-30 17:15:29 -0300

    Tela de História 1.2
    Adição das imagens e dos painéis. Adicionado tbm uma função de timer para ficar trocando as imagens em um painel

--- SoftwarePIM/Frm_TelaHistoria.Designer.cs
+++ SoftwarePIM/Frm_TelaHistoria.Designer.cs
@@ -28,17 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Frm_TelaHistoria));
             btn_Anterior = new PictureBox();
             btn_Proximo = new PictureBox();
-            pictureBox3 = new PictureBox();
-            label1 = new Label();
-            label2 = new Label();
             btn_Menu = new PictureBox();
+            panel1 = new Panel();
+            label2 = new Label();
+            label1 = new Label();
+            pictureBox3 = new PictureBox();
+            panel2 = new Panel();
+            lbl_Obra1 = new Label();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
+            timer1 = new System.Windows.Forms.Timer(components);
             ((System.ComponentModel.ISupportInitialize)btn_Anterior).BeginInit();
             ((System.ComponentModel.ISupportInitialize)btn_Proximo).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
             ((System.ComponentModel.ISupportInitialize)btn_Menu).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // btn_Anterior
@@ -54,6 +64,7 @@
             btn_Anterior.SizeMode = PictureBoxSizeMode.Zoom;
             btn_Anterior.TabIndex = 0;
             btn_Anterior.TabStop = false;
+            btn_Anterior.Click += btn_Anterior_Click;
             // 
             // btn_Proximo
             // 
@@ -68,50 +79,7 @@
             btn_Proximo.SizeMode = PictureBoxSizeMode.Zoom;
             btn_Proximo.TabIndex = 1;
             btn_Proximo.TabStop = false;
-            //btn_Proximo.Click += btn_Proximo_Click;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.BackColor = Color.Transparent;
-            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
-            pictureBox3.BackgroundImageLayout = ImageLayout.Zoom;
-            pictureBox3.Location = new Point(146, 85);
-            pictureBox3.Margin = new Padding(3, 2, 3, 2);
-            pictureBox3.MaximumSize = new Size(156, 242);
-            pictureBox3.MinimumSize = new Size(156, 242);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(156, 242);
-            pictureBox3.TabIndex = 2;
-            pictureBox3.TabStop = false;
-            // 
-            // label1
-            // 
-            label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.ForeColor = Color.FromArgb(0, 192, 192);
-            label1.Location = new Point(308, 85);
-            label1.MaximumSize = new Size(324, 32);
-            label1.MinimumSize = new Size(324, 32);
-            label1.Name = "label1";
-            label1.RightToLeft = RightToLeft.No;
-            label1.Size = new Size(324, 32);
-            label1.TabIndex = 3;
-            label1.Text = "\"UM PEQUENO PASSO PARA O HOMEM, UM GRANDE PASSO PARA A HUMANIDADE\"";
-            label1.TextAlign = ContentAlignment.TopCenter;
-            label1.UseMnemonic = false;
-            // 
-            // label2
-            // 
-            label2.BackColor = Color.Transparent;
-            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
-            label2.ForeColor = Color.White;
-            label2.Location = new Point(318, 127);
-            label2.MaximumSize = new Size(314, 200);
-            label2.MinimumSize = new Size(314, 200);
-            label2.Name = "label2";
-            label2.Size = new Size(314, 200);
-            label2.TabIndex = 4;
-            label2.Text = resources.GetString("label2.Text");
+            btn_Proximo.Click += btn_Proximo_Click;
             // 
             // btn_Menu
             // 
@@ -128,6 +96,118 @@
             btn_Menu.TabStop = false;
             btn_Menu.Click += btn_Menu_Click;
             // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Transparent;
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox3);
+            panel1.Location = new Point(138, 71);
+            panel1.MaximumSize = new Size(504, 255);
+            panel1.MinimumSize = new Size(504, 255);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(504, 255);
+            panel1.TabIndex = 6;
+            // 
+            // label2
+            // 
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(181, 48);
+            label2.MaximumSize = new Size(314, 200);
+            label2.MinimumSize = new Size(314, 200);
+            label2.Name = "label2";
+            label2.Size = new Size(314, 200);
+            label2.TabIndex = 7;
+            label2.Text = resources.GetString("label2.Text");
+            // 
+            // label1
+            // 
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(0, 192, 192);
+            label1.Location = new Point(171, 6);
+            label1.MaximumSize = new Size(324, 32);
+            label1.MinimumSize = new Size(324, 32);
+            label1.Name = "label1";
+            label1.RightToLeft = RightToLeft.No;
+            label1.Size = new Size(324, 32);
+            label1.TabIndex = 6;
+            label1.Text = "\"UM PEQUENO PASSO PARA O HOMEM, UM GRANDE PASSO PARA A HUMANIDADE\"";
+            label1.TextAlign = ContentAlignment.TopCenter;
+            label1.UseMnemonic = false;
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackColor = Color.Transparent;
+            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
+            pictureBox3.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox3.Location = new Point(9, 6);
+            pictureBox3.Margin = new Padding(3, 2, 3, 2);
+            pictureBox3.MaximumSize = new Size(156, 242);
+            pictureBox3.MinimumSize = new Size(156, 242);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(156, 242);
+            pictureBox3.TabIndex = 5;
+            pictureBox3.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Transparent;
+            panel2.Controls.Add(lbl_Obra1);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Location = new Point(138, 71);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(504, 255);
+            panel2.TabIndex = 8;
+            panel2.Visible = false;
+            // 
+            // lbl_Obra1
+            // 
+            lbl_Obra1.BackColor = Color.Transparent;
+            lbl_Obra1.Font = new Font("Segoe UI", 9.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lbl_Obra1.ForeColor = Color.White;
+            lbl_Obra1.Location = new Point(181, 48);
+            lbl_Obra1.MaximumSize = new Size(314, 200);
+            lbl_Obra1.MinimumSize = new Size(314, 200);
+            lbl_Obra1.Name = "lbl_Obra1";
+            lbl_Obra1.Size = new Size(314, 200);
+            lbl_Obra1.TabIndex = 7;
+            lbl_Obra1.Text = resources.GetString("lbl_Obra1.Text");
+            // 
+            // label4
+            // 
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = Color.FromArgb(0, 192, 192);
+            label4.Location = new Point(171, 6);
+            label4.MaximumSize = new Size(324, 32);
+            label4.MinimumSize = new Size(324, 32);
+            label4.Name = "label4";
+            label4.RightToLeft = RightToLeft.No;
+            label4.Size = new Size(324, 32);
+            label4.TabIndex = 6;
+            label4.Text = "MINIATURA DA LUA";
+            label4.TextAlign = ContentAlignment.MiddleCenter;
+            label4.UseMnemonic = false;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(9, 6);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.MaximumSize = new Size(156, 242);
+            pictureBox1.MinimumSize = new Size(156, 242);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(156, 242);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
             // Frm_TelaHistoria
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
@@ -135,10 +215,9 @@
             BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
             BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(784, 561);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Controls.Add(btn_Menu);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(pictureBox3);
             Controls.Add(btn_Proximo);
             Controls.Add(btn_Anterior);
             DoubleBuffered = true;
@@ -152,8 +231,11 @@
             Load += Frm_TelaHistoria_Load;
             ((System.ComponentModel.ISupportInitialize)btn_Anterior).EndInit();
             ((System.ComponentModel.ISupportInitialize)btn_Proximo).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
             ((System.ComponentModel.ISupportInitialize)btn_Menu).EndInit();
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -161,9 +243,15 @@
 
         private PictureBox btn_Anterior;
         private PictureBox btn_Proximo;
-        private PictureBox pictureBox3;
-        private Label label1;
-        private Label label2;
         private PictureBox btn_Menu;
+        private Panel panel1;
+        private Label label2;
+        private Label label1;
+        private PictureBox pictureBox3;
+        private Panel panel2;
+        private Label lbl_Obra1;
+        private Label label4;
+        private PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
--- SoftwarePIM/Frm_TelaHistoria.cs
+++ SoftwarePIM/Frm_TelaHistoria.cs
@@ -12,15 +12,47 @@ namespace SoftwarePIM
 {
     public partial class Frm_TelaHistoria : Form
     {
+        public Form? FormAnterior { get; set; }
+        public Form? FormPrincipal { get; set; }
+
+        // Array de caminhos dos arquivos de imagem no seu PC
+        private string[] caminhosDasImagens = {
+            @"C:\Users\t_matheus.caetano\Documents\Dev\pim3\SoftwarePIM\Properties\imagens\Obra5.1.jpg",
+            @"C:\Users\t_matheus.caetano\Documents\Dev\pim3\SoftwarePIM\Properties\imagens\Obra5.2.jpg",
+            @"C:\Users\t_matheus.caetano\Documents\Dev\pim3\SoftwarePIM\Properties\imagens\Obra5.3.jpg"
+        };
+
+        private int indiceAtual = 0;
+
         public Frm_TelaHistoria()
         {
             InitializeComponent();
+
+            // Inicialize o Timer
+            timer1.Interval = 3000; // Intervalo em milissegundos (3 segundos neste exemplo)
+            timer1.Tick += Timer1_Tick;
+
+            // Exiba a primeira imagem
+            pictureBox1.Image = Image.FromFile(caminhosDasImagens[indiceAtual]);
+
+            // Inicie o Timer
+            timer1.Start();
+
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            // Troque para a próxima imagem
+            indiceAtual = (indiceAtual + 1) % caminhosDasImagens.Length;
+            pictureBox1.Image = Image.FromFile(caminhosDasImagens[indiceAtual]);
         }
 
         private void btn_Menu_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
+        private int painelAtual = 1; // Variável para rastrear qual painel está atualmente visível
 
         //private void btn_Proximo_Click(object sender, EventArgs e)
         //{
@@ -41,7 +73,89 @@ namespace SoftwarePIM
             this.FormPrincipal?.Show();
         }
 
-        public Form? FormAnterior { get; set; }
-        public Form? FormPrincipal { get; set; }
+        private void btn_Proximo_Click(object sender, EventArgs e)
+        {
+
+            // Esconde o painel atual
+            switch (painelAtual)
+            {
+                case 1:
+                    panel1.Visible = false;
+                    break;
+                case 2:
+                    panel2.Visible = false;
+                    break;
+                //case 3:
+                //    panel3.Visible = false;
+                //    break;
+                //case 4:
+                //    panel4.Visible = false;
+                //    break;
+                //case 5:
+                //    panel5.Visible = false;
+                //    break;
+
+            }
+
+            // Passa para o próximo painel
+            painelAtual++;
+            if (painelAtual > 2)
+                painelAtual = 1;
+
+            // Exibe o próximo painel
+            switch (painelAtual)
+            {
+                case 1:
+                    panel1.Visible = true;
+                    break;
+                case 2:
+                    panel2.Visible = true;
+                    break;
+                //case 3:
+                //    panel3.Visible = true;
+                //    break;
+                //case 4:
+                //    panel4.Visible = true;
+                //    break;
+                //case 5:
+                //    panel5.Visible = true;
+                //    break;
+            }
+
+        }
+
+        private void btn_Anterior_Click(object sender, EventArgs e)
+        {
+            // Esconde o painel atual
+            switch (painelAtual)
+            {
+                case 1:
+                    panel1.Visible = false;
+                    break;
+                case 2:
+                    panel2.Visible = false;
+                    break;
+                    // Adicione os casos necessários para os painéis adicionais aqui
+            }
+
+            // Volta para o painel anterior
+            painelAtual--;
+            if (painelAtual < 1)
+                painelAtual = 2; // Altere para o número total de painéis
+
+            // Exibe o painel anterior
+            switch (painelAtual)
+            {
+                case 1:
+                    panel1.Visible = true;
+                    break;
+                case 2:
+                    panel2.Visible = true;
+                    break;
+                    // Adicione os casos necessários para os painéis adicionais aqui
+            }
+        }
+
+        
     }
 }
